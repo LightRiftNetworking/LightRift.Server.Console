92e024d1ee3eae10cbfb3e050557c285bc0d433d
Kapitan Mliko <vit.hrciak@virtuplex.cz>
2024-10-01 11:27:03 +0200

    net8 console app, sln

---
+++ DarkRift.Server.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DarkRift.Server.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
